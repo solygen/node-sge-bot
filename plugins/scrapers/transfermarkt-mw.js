8b20f5a81a3363071899f65ffc40de18cceeadb7
frank@solygen.de <frank@solygen.de>
2015-04-26 21:34:18 +0200

    unlicense; add tmmw source;

---
+++ plugins/scrapers/transfermarkt-mw.js
@@ -0,0 +1,46 @@
+(function () {
+
+    'use strict';
+
+    module.exports =  {
+        url: 'http://www.transfermarkt.de/eintracht-frankfurt/marktwertanalyse/verein/24',
+        name: 'tmmw',
+        icon: '',
+        selector: {
+            'titles[]': '.spielprofil_tooltip',
+            'images[]': '.bilderrahmen@src',
+            'links[]': '.spielprofil_tooltip@href',
+            'contents[]': '.rechts.hauptlink',
+            'trends[]': '.rechts.hauptlink>span@class'
+        },
+        hashtags: ['tm'],
+        extract: function (def, data) {
+            var titles = data.titles,
+                links = data.links,
+                images = data.images,
+                contents = data.contents,
+                trends = data.trends,
+                list = [];
+
+
+            titles.forEach(function (title, index) {
+                var trend = trends[index];
+
+                if (trend.indexOf('grey') >= 0) return;
+
+                trend = trend.indexOf('green') >= 0 ? '+' : '-';
+
+                list.push({
+                    title: title + ': ' + contents[index] + ' (' + trend + ')',
+                    content: contents[index],
+                    short: title.slice(0,140),
+                    source: 'tmmw',
+                    url: 'http://www.transfermarkt.de' + links[index]
+                });
+            });
+
+            def.resolve(list);
+        }
+    };
+
+}());
