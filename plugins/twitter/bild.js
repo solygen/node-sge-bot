2304ca89fd93a71ca7c477e096d81bf51ad26f09
frank@solygen.de <frank@solygen.de>
2015-06-30 19:18:58 +0200

    add bild scraper

---
+++ plugins/twitter/bild.js
@@ -0,0 +1,14 @@
+(function () {
+
+    'use strict';
+
+    var _ = require('lodash');
+
+    module.exports =  {
+        id: 'bild_eintracht',
+        name: 'twitter-bild',
+        filter: function (list) {
+            return list;
+        }
+    };
+}());
