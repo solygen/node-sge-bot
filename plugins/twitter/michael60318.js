c73c2feec2a97de3e8448a4fea5584f1c973b756
Frank Paczynski <frank.paczynski@solygen.de>
2016-06-05 16:35:11 +0200

    fix source Michael Ebert

--- plugins/twitter/michael60318.js
+++ plugins/twitter/michael60318.js
@@ -2,12 +2,17 @@
 
     'use strict';
 
-    var util = require('../../lib/util');
+    var _ = require('lodash'),
+        util = require('../../lib/util');
 
     module.exports =  {
         id: 'michael60318',
         name: 'twitter-michaelebert',
-        filter: util.relevant
+        filter: function (list) {
+            return _.filter(list, function (item) {
+                return util.isRelevant(title) && !/\S*kicker\.de\S*/.test(title);
+            });
+        }
     };
 
 }());
